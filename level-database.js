d5a6a6e98261f5e51a1026c0f50e755e3a440b30
Stanny Huang <hstanny@gmail.com>
2025-11-06 13:18:29 -0500

    Added basic levels to the database

--- level-database.js
+++ level-database.js
@@ -7,51 +7,51 @@ export const levels = [
     },
     {
         monsterSprite: "SKELETON_PLACEHOLDER",
-        solution: "test",
+        solution: "divine beam",
         background: "BACKGROUND_PLACEHOLDER"
     },
 
     //double level
     {
         monsterSprite: "src/monsters/shadow_slime.png",
-        solution: "test",
+        solution: "divine beam",
         background: "src/backgrounds/grave_graveyards.png"
     },
     {
         monsterSprite: "src/monsters/fire_slime.png",
-        solution: "test",
+        solution: "water cannon",
         background: "src/backgrounds/grave_graveyards.png"
     },
     //
 
     {
         monsterSprite: "src/monsters/pit_ghast.png",
-        solution: "test",
+        solution: "ice shard",
         background: "src/backgrounds/volcano.png"
     },
     {
         monsterSprite: "src/monsters/noughtsquid.png",
-        solution: "test",
+        solution: "extinguish",
         background: "src/backgrounds/ocean.png"
     },
         {
         monsterSprite: "NONE",
-        solution: "test",
+        solution: "boulder roll",
         background: "src/backgrounds/criptid.png"
     },
         {
         monsterSprite: "MINIBOSS_PLACEHOLDER",
-        solution: "test",
+        solution: "fiery shackles of imprisonment",
         background: "src/backgrounds/volcano.png"
     },
         {
         monsterSprite: "TRIPTYCH_PLACEHOLDER",
-        solution: "test",
+        solution: "azimuth blight",
         background: "src/backgrounds/grave_graveyards.png"
     },
     {
         monsterSprite: "src/monsters/bone_golem.png",
-        solution: "test",
+        solution: "as above, so below",
         background: "src/backgrounds/bonewood_forest.png"
     }
 ];
