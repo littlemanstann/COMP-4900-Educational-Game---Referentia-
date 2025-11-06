69b413c861c29513e686433c07cc3e97d146b3ba
GreenPixel10 <evan@raz-family.ca>
2025-11-06 00:52:19 -0500

    updated level database and added new sprites

--- level-database.js
+++ level-database.js
@@ -6,14 +6,53 @@ export const levels = [
         background: "src/backgrounds/ice_plains.png"
     },
     {
-        monsterSprite: "src/monsters/bone_golem.png",
-        solution: "shield",
+        monsterSprite: "SKELETON_PLACEHOLDER",
+        solution: "test",
+        background: "BACKGROUND_PLACEHOLDER"
+    },
+
+    //double level
+    {
+        monsterSprite: "src/monsters/shadow_slime.png",
+        solution: "test",
         background: "src/backgrounds/grave_graveyards.png"
     },
     {
-        monsterSprite: "src/monsters/dragon.png",
-        solution: "iceblast",
-        background: "src/backgrounds/background3.png"
+        monsterSprite: "src/monsters/fire_slime.png",
+        solution: "test",
+        background: "src/backgrounds/grave_graveyards.png"
+    },
+    //
+
+    {
+        monsterSprite: "src/monsters/pit_ghast.png",
+        solution: "test",
+        background: "src/backgrounds/volcano.png"
+    },
+    {
+        monsterSprite: "src/monsters/noughtsquid.png",
+        solution: "test",
+        background: "src/backgrounds/ocean.png"
+    },
+        {
+        monsterSprite: "LEAVE_INVALID",
+        solution: "test",
+        background: "src/backgrounds/criptid.png"
+    },
+        {
+        monsterSprite: "MINIBOSS_PLACEHOLDER",
+        solution: "test",
+        background: "src/backgrounds/volcano.png"
+    },
+        {
+        monsterSprite: "TRIPTYCH_PLACEHOLDER",
+        solution: "test",
+        background: "src/backgrounds/grave_graveyards.png"
+    },
+    {
+        monsterSprite: "src/monsters/bone_golem.png",
+        solution: "test",
+        background: "src/backgrounds/bonewood_forest.png"
     }
 ];
 
